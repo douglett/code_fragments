61db6db1cd0e90b9f33b24f06eca6ea0d8a71d42
douglett@home <doug@totem9.net>
2015-11-06 01:09:51 +0000

    working on paring to list

--- tinylisp/main.cpp
+++ tinylisp/main.cpp
@@ -6,14 +6,14 @@
 #include <sstream>
 #include <regex>
 
-#include "symbol.h"
+#include "token.h"
 
 using namespace std;
 
 
-vector<Symbol> symbollist;
+vector<Token> tokenlist;
 
-vector<vector<string> > symbol_expressions = {
+vector<vector<string> > token_expressions = {
 	{ "(", "\\(" },
 	{ ")", "\\)" },
 	{ "integer", "[0-9]+" },
@@ -23,15 +23,15 @@ vector<vector<string> > symbol_expressions = {
 
 
 int exprid(string name) {
-	for (int i = 0; i < symbol_expressions.size(); i++)
-		if (symbol_expressions[i][0] == name)
+	for (int i = 0; i < token_expressions.size(); i++)
+		if (token_expressions[i][0] == name)
 			return i;
 	return -1;
 }
 string exprname(int id) {
-	if (id < 0 || id >= symbol_expressions.size())
+	if (id < 0 || id >= token_expressions.size())
 		return "undefined";
-	return symbol_expressions[id][0];
+	return token_expressions[id][0];
 }
 
 
@@ -42,18 +42,19 @@ int match(string s, string e) {
 
 
 int match_any(string s) {
-	for (int i = 0; i < symbol_expressions.size(); i++) {
-		if (match(s, symbol_expressions[i][1]))
+	for (int i = 0; i < token_expressions.size(); i++) {
+		if (match(s, token_expressions[i][1]))
 			return i;
 	}
 	return -1;
 }
 
 
-void parse_symbols_string(string symbolstr) {
-	stringstream ss(symbolstr);
+void parse_token_string(string tokstr, int line, int linepos) {
+	stringstream ss(tokstr);
 	string s;
 	char c;
+	int pos = 1;
 
 	while ((c = ss.peek()) != EOF || s.length() > 0) {
 		// cout << c << endl;
@@ -62,13 +63,15 @@ void parse_symbols_string(string symbolstr) {
 			s += ss.get();
 		} else {
 			if (s.length() > 0) {
-				// cout << "\t" << s << endl;
 				m = match_any(s);
-				symbollist.push_back(Symbol(m, s));
+				tokenlist.push_back(Token(m, s, line, linepos+pos));
+				pos += s.length();
+				// cout << "\t" << s << endl;
 			} else {
 				c = ss.get();
+				tokenlist.push_back(Token(-1, string(&c, 1), line, linepos+pos));
+				pos += 1;
 				// cout << "\t" << "[" << c << "]?" << endl;
-				symbollist.push_back(Symbol(-1, string(&c, 1)));
 			}
 			s = "";
 		}
@@ -76,43 +79,103 @@ void parse_symbols_string(string symbolstr) {
 }
 
 
-void parse_symbols(string fname) {
+void parse_token_file(string fname) {
 	fstream f(fname);
+	stringstream ss;
 	string s;
+	int line = 0;
 
-	while (f >> s) {
-		parse_symbols_string(s);
+	while (getline(f, s)) {
+		line++;
+		ss.str(s), ss.clear();
+		int linepos = 1;
+		while (ss >> s) {
+			// printf("%d 	%d\n", line, linepos);
+			parse_token_string(s, line, linepos);
+			linepos = ss.tellg();
+		}
 	}
 
 	f.close();
 }
 
 
-void show_symbols() {
-	for (auto sym : symbollist) {
-		cout << setw(10) << exprname(sym.type) << " : " << sym.val << endl;
+void show_tokens() {
+	for (auto tok : tokenlist) {
+		cout << left 
+			<< setw(14) << exprname(tok.type) 
+			<< setw(14) << tok.val 
+			<< tok.line << "-" << tok.pos 
+			<< endl;
 	}
 }
 
 
 int expect(string s, int pos) {
 	int id = exprid(s);
-	if (id == symbollist[pos].type)
+	if (id == tokenlist[pos].type)
 		return 1;
 	return 0;
 }
 
 
-int eval(int startpos) {
+// int eval(int startpos) {
+// 	int pos = startpos;
+// 	expect("(", pos);
+// 	expect(")", pos);
+// 	return 0;
+// }
+
+
+class val {
+public:
+	enum TYPES {
+		T_LIST,
+		T_INT
+	};
+	int type = 0;
+	int ival = 0;
+	vector<val> lval;
+};
+
+
+val parse_list(int startpos, int* len) {
 	int pos = startpos;
-	expect("(", pos);
-	expect(")", pos);
+	val v;
+	if (!expect("(", pos))
+		return v;
+	pos++;
+
+	while (pos < tokenlist.size() && !expect(")", pos)) {
+		if (expect("(", pos)) {
+			int len = 0;
+			parse_list(pos, &len);
+			pos += len;
+		} else {
+			cout << tokenlist[pos].val << endl;
+			pos++;
+		}
+	}
+
+	if (!expect(")", pos)) {
+		cout << "error: missing end bracket" << endl;
+		if (len != NULL)
+			*len = -1;
+	} else {
+		pos++;
+		if (len != NULL)
+			*len = pos - startpos;
+	}
+
+	return v;
 }
 
 
 int main() {
 	cout << "hello" << endl;
-	parse_symbols("doug.lisp");
-	show_symbols();
-	eval(0);
+	parse_token_file("doug.lisp");
+	show_tokens();
+	cout << endl;
+	val v = parse_list(0, NULL);
+	cout << endl;
 }
--- tinylisp/token.h
+++ tinylisp/token.h
@@ -3,13 +3,17 @@
 
 #include <string>
 
-class Symbol {
+class Token {
 public:
 	int type = 0;
 	std::string val;
-	Symbol(int type, std::string val) {
+	int line = 0;
+	int pos = 0;
+	Token(int type, std::string val, int line, int pos) {
 		this->type = type;
 		this->val = val;
+		this->line = line;
+		this->pos = pos;
 	}
 };
 
