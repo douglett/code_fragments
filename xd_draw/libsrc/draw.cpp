d3419a841f0370d6a6040f5e65b9e65f65a042be
dougdoug <doug.inman@probability.co.uk>
2016-05-10 17:21:04 +0100

    added some drawing primatives

--- xd_draw/libsrc/draw.cpp
+++ xd_draw/libsrc/draw.cpp
@@ -40,6 +40,47 @@ namespace draw {
 		return 0;
 	}
 
+	int tracerect(uint32_t* imgdata, uint32_t color, int posx, int posy, int w, int h) {
+		int img_width = imgdata[0];
+		int img_height = imgdata[1];
+		int maxw = posx + w - 1;
+		int maxh = posy + h - 1;
+		if (posy >= 0)
+			for (int x = max(posx, 0); x < min(posy+w, img_width); x++)
+				imgdata[2 + x] = color;
+		if (maxh < img_height)
+			for (int x = max(posx, 0); x < min(posy+w, img_width); x++)
+				imgdata[2 + img_width*maxh + x] = color;
+		if (posx >= 0)
+			for (int y = max(posy, 0); y < min(posy+h, img_height); y++)
+				imgdata[2 + img_width*y] = color;
+		if (maxw < img_width)
+			for (int y = max(posy, 0); y < min(posy+h, img_height); y++)
+				imgdata[2 + img_width*y + maxw] = color;
+		return 0;
+	}
+
+	int traceline(uint32_t* imgdata, uint32_t color, int x1, int y1, int x2, int y2) {
+		int img_width = imgdata[0];
+		int img_height = imgdata[1];
+		if (x1 > x2) swap(x1, x2);
+		if (y1 > y2) swap(y1, y2);
+		int dx = x2 - x1;
+		int dy = y2 - y1;
+		int steps = max(dx, dy);
+		double stepx = dx / double(steps);
+		double stepy = dy / double(steps);
+
+		for (int i = 0; i < steps; i++) {
+			int x = x1 + int(stepx * i);
+			int y = y1 + int(stepy * i);
+			if (x > 0 && y > 0 && x < img_width && y < img_height)
+				imgdata[2 + img_width*y + x] = color;
+		}
+		
+		return 0;
+	}
+
 	int blit(uint32_t* src, uint32_t* dst, int posx, int posy) {
 		int src_width = src[0],
 			src_height = src[1],
--- xd_draw/main.cpp
+++ xd_draw/main.cpp
@@ -14,6 +14,11 @@ int main() {
 
 	xd::screen::init();
 
+	// make hard sprite
+	screen::Sprite* s = screen::makesprite(20, 20);
+	s->pos(25, 25);
+	draw::fillrect(s->getdata(), draw::rgb(150, 150, 0), 0, 0, 20, 20);
+
 	// make a sprite
 	uint32_t* img = draw::make_img(20, 20);
 	draw::fillrect(img, draw::rgb(255, 255, 0), 0, 0, 20, 20);
@@ -28,6 +33,13 @@ int main() {
 		draw::fillrect(scr, draw::rgb(255, 0, 0), -5, -5, 40, 40);
 		draw::fillrect(scr, draw::rgb(0, 255, 0), 40, 40, 40, 40);
 		draw::fillrect(scr, draw::rgb(0, 0, 255), 80, 80, 40, 40);
+
+		draw::tracerect(scr, draw::rgb(255, 0, 0), 0, 0, 240, 160);
+		draw::tracerect(scr, draw::rgb(255, 0, 0), -1, -1, 500, 500);
+
+		draw::traceline(scr, draw::rgb(255, 0, 0), 100, 10, 100+80, 10+30);
+		draw::traceline(scr, draw::rgb(255, 0, 0), 100, 10, 100+30, 10+80);
+		draw::traceline(scr, draw::rgb(255, 0, 0), 100, 10, 100-30, 10-15);
 
 		// draw sprite		
 		draw::blit(img, scr, posx, posy);
