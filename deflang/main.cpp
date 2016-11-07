c704cce0c323662a8d862e1b078e3551ea011e1d
dougdoug <doug.inman@probability.co.uk>
2016-11-07 11:53:26 +0000

    def work

--- deflang/main.cpp
+++ deflang/main.cpp
@@ -8,15 +8,30 @@ using namespace std;
 
 
 const regex REG_DEFINE("#define", regex::icase);
-const regex REG_MACRO ("#macro",  regex::icase);
+const regex REG_EXPR  ("#expr",   regex::icase);
+// const regex REG_MACRO ("#macro",  regex::icase);
 const regex REG_IDENT ("[a-z][a-z0-9_-]*", regex::icase);
 const regex REG_INT   ("[-+]?[0-9]+");
 struct deftype {
-	int type;
+	char type;
 	regex regex;
 	vector<string> vs;
 };
 map<string, deftype> deflist;
+
+int parse_file(char type, const string& fname);
+int parse_deflist_line(int lineno, const string& ln);
+int parse_input_line(int lineno, const string& ln);
+
+
+//--- main ---
+
+int main() {
+	int err = 0;
+	err = parse_file('d', "deflist.txt");
+	err = parse_file('i', "input.txt");
+	return err;
+}
 
 
 //--- utilities ---
@@ -44,7 +59,7 @@ void chomp(string& s) {
 
 //--- parsing ---
 
-int parse_file(const string& fname) {
+int parse_file(char type, const string& fname) {
 	// open file
 	printf("parsing: %s \n", fname.c_str());
 	ifstream ifs(fname);
@@ -53,63 +68,102 @@ int parse_file(const string& fname) {
 		return 1;
 	}
 	// prepare loop
-	stringstream ss;
 	string s;
-	int lineno = 0;
+	int lineno = 0, err = 0;
 	// grab each line from file
 	while (getline(ifs, s)) {
-		string meta, ident;
 		lineno++;
-		ss.str(s), ss.clear();
-		// get meta-command
-		ss >> meta;
-		if (meta.length() == 0)  continue;  // ignore blank lines
-		if (meta[0] == ';')      continue;  // ignore comment lines
-		if (regex_match(meta, REG_DEFINE) || regex_match(meta, REG_MACRO)) {
-			// check for regex name
-			ss >> ident;
-			if (!regex_match(ident, REG_IDENT)) {
-				fprintf(stderr, "missing identifier : l%03d : [%s] \n", lineno, ident.c_str());
-				return 1;
-			} else if (deflist.find(ident) != deflist.end()) {
-				fprintf(stderr, "identifier already defined : l%03d : [%s] \n", lineno, ident.c_str());
-				return 1;
-			}
-			// get define regex
-			if (regex_match(meta, REG_DEFINE)) {
-				string mdef;
-				getline(ss, mdef);
-				chomp(mdef);
-				deflist[ident] = { .type = 0, .regex = regex(mdef) };
-				printf("    l%03d:  [%-15s]  [%s] \n", lineno, ident.c_str(), mdef.c_str());
-			}
-			// get define macro
-			else if (regex_match(meta, REG_MACRO)) {
-				vector<string> vs;
-				while (ss >> s) {
-					if (deflist.find(s) == deflist.end()) {
-						fprintf(stderr, "macro contains unknown definition : l%03d : [%s] \n", lineno, ident.c_str());
-						return 1;
-					}
-					vs.push_back(s);
-				}
-				deflist[ident] = { .type = 1, .vs = vs };
-				printf("    l%03d:  [%-15s]  [%d defines] \n", lineno, ident.c_str(), (int)vs.size());
-			}
-		} else {
-			fprintf(stderr, "bad meta-command : l%03d : [%s] \n", lineno, meta.c_str());
+		if (s.length() == 0)  continue;  // ignore blank lines
+		if (s[0] == ';')      continue;  // ignore comment lines
+		if (type == 'd')  parse_deflist_line(lineno, s);
+		if (type == 'i')  parse_input_line(lineno, s);
+		if (err)  return err;
+	}
+	return 0;
+}
+
+
+int parse_deflist_line(int lineno, const string& ln) {
+	string meta, ident;
+	stringstream ss(ln);
+	// get meta-command
+	ss >> meta;
+	if (regex_match(meta, REG_DEFINE) || regex_match(meta, REG_EXPR)) {
+		// check for regex name
+		ss >> ident;
+		if (!regex_match(ident, REG_IDENT)) {
+			fprintf(stderr, "missing identifier : l%03d : [%s] \n", lineno, ident.c_str());
+			return 1;
+		} else if (deflist.find(ident) != deflist.end()) {
+			fprintf(stderr, "identifier already defined : l%03d : [%s] \n", lineno, ident.c_str());
 			return 1;
 		}
-	} // end line loop
+		// get define regex
+		if (regex_match(meta, REG_DEFINE)) {
+			string mdef;
+			getline(ss, mdef);
+			chomp(mdef);
+			deflist[ident] = { .type = 'd', .regex = regex(mdef) };
+			printf("    l%03d:  [%-15s]  [%s] \n", lineno, ident.c_str(), mdef.c_str());
+		}
+		// get define expression
+		else if (regex_match(meta, REG_EXPR)) {
+			string s;
+			vector<string> vs;
+			while (ss >> s) {
+				if (deflist.find(s) == deflist.end()) {
+					fprintf(stderr, "expression contains unknown definition : l%03d : [%s] \n", lineno, ident.c_str());
+					return 1;
+				}
+				vs.push_back(s);
+			}
+			deflist[ident] = { .type = 'e', .vs = vs };
+			printf("    l%03d:  [%-15s]  [%d defines] \n", lineno, ident.c_str(), (int)vs.size());
+		}
+	} else {
+		fprintf(stderr, "bad meta-command : l%03d : [%s] \n", lineno, meta.c_str());
+		return 1;
+	}
 	return 0;  // success (destroys fstream automatically)
 }
 
 
-int main() {
-	// regex r(".*asd.*");
-	// cout << "regex test: " << regex_match("blhasdaf", r) << endl;
-	// cout << "regex test: " << regex_match("10", REG_INT) << endl;
-
-	int err = parse_file("deflist.txt");
-	return err;
+int parse_input_line(int lineno, const string& ln) {
+	stringstream ss(ln);
+	string tok;
+	vector<pair<string, string> > toklist;
+	// find all tokens
+	while (ss >> tok) {
+		int found = 0;
+		for (const auto& d : deflist)
+			if (d.second.type == 'd' && regex_match(tok, d.second.regex)) {
+				toklist.push_back({ d.first, tok });
+				found = 1;
+				break;
+			}
+		if (!found) {
+			fprintf(stderr, "token not found : l%03d : %s \n", lineno, tok.c_str());
+			return 1;
+		}
+	}
+	// make expressions from token list
+	string en;
+	for (const auto& d : deflist) {
+		if (d.second.type != 'e')  continue;
+		if (d.second.vs.size() != toklist.size())  continue;
+		int found = 1;
+		for (int i = 0; i < toklist.size(); i++)
+			if (d.second.vs[i] != toklist[i].first) {
+				found = 0;
+				break;
+			}
+		if (found) {
+			en = d.first;
+			break;
+		}
+	}
+	// 
+	if (en.size())
+		cout << en << endl;
+	return 0;
 }
