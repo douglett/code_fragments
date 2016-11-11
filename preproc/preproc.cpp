796df84ce24c3e025ceff26a82db9c636202ae29
dougdoug <doug.inman@probability.co.uk>
2016-11-11 17:30:10 +0000

    new parse type

--- preproc/preproc.cpp
+++ preproc/preproc.cpp
@@ -6,18 +6,21 @@
 using namespace std;
 
 
-// header
+// helpers
 void strtolower(string& s);
 void chomp(string& s);
 int  is_meta(const string& s);
 string strhash(const string& s);
-int  replace_labels(string& line, const string& mpattern);
+int  regex_replace_cb(string& line, const regex& REG, void (*cb)(string& s));
+// func
+void label_cb(string& s);
 // consts
 const regex REG_IDENT      ("[a-z][a-z0-9_-]*",      regex::icase);
 const regex REG_LABEL      (":[a-z_][a-z0-9_]*",     regex::icase);
 const regex REG_INT        ("[-+]?[0-9]+");
 // member
 map<string, string> deflist;
+string mpattern;
 
 
 
@@ -25,7 +28,7 @@ int main() {
 	vector<string> fnames = { "test1.dasm16", "test2.dasm16" };
 	ofstream ofs("test_out.dasm16");
 	// ostream& ofs = cout;
-	string s, mpattern;
+	string s;
 	for (const auto& fn : fnames) {
 		ifstream ifs(fn);
 		int lineno = 0;
@@ -35,34 +38,18 @@ int main() {
 			if (is_meta(s)) {
 				// parse_meta(s);  // save meta-command
 			} else { 
-				replace_labels(s, mpattern);   // mangle
+				regex_replace_cb(s, REG_LABEL, label_cb);  // mangle
 			}
-			ofs << s;  // output
-			if (!ifs.eof())  ofs << endl;  // manual end lines
+			ofs << s << endl;  // output
 			// cout << s << ( is_meta(s) ? "  (is_meta)" : "" ) << endl;  // show
 		}
 	}
 }
 
 
-int replace_labels(string& line, const string& mpattern) {
-	int count = 0;
-	smatch match;
-	auto it = line.cbegin();
-	string s;
-	while (regex_search(it, line.cend(), match, REG_LABEL)) {
-		count++;
-		auto& tok = match[0];
-		int pos = distance(line.cbegin(), tok.first);
-		s = tok.str();
-		if (s[1] < 'A' || s[1] > 'Z') {
-			// printf("-- %d  %s  %s  %d \n", pos, tok.str().c_str(), s.c_str(), (int)s.length());
-			s = mpattern + s.substr(1);
-			line.replace(tok.first, tok.second, s);
-		}
-		it = line.cbegin() + pos + s.length();
-	}
-	return count;
+void label_cb(string& s) {
+	if (s[1] < 'A' || s[1] > 'Z')
+		s = mpattern + s.substr(1);
 }
 
 
@@ -106,4 +93,22 @@ string strhash(const string& s) {
 	int i = mhash("s");
 	snprintf(cs, 6, "%0x", i);
 	return cs;
+}
+
+// regex replace with callback, js style
+int regex_replace_cb(string& line, const regex& REG, void (*cb)(string& s)) {
+	int count = 0;
+	smatch match;
+	auto it = line.cbegin();
+	string s;
+	while (regex_search(it, line.cend(), match, REG)) {
+		count++;
+		auto& tok = match[0];
+		int pos = distance(line.cbegin(), tok.first);
+		s = tok.str();
+		cb(s);
+		line.replace(tok.first, tok.second, s);
+		it = line.cbegin() + pos + s.length();
+	}
+	return count;
 }
