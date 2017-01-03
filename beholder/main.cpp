6dd2ab3102efa5a65330b6d9680a8898fc04fb14
dougdoug <doug.inman@probability.co.uk>
2017-01-03 11:00:19 +0000

    new tracerects

--- beholder/main.cpp
+++ beholder/main.cpp
@@ -15,35 +15,17 @@ int main() {
 
 	uint32_t c = 0xffffffff;
 	// left row1
-	// xd::draw::traceline(dat, 0, 0, 0, 99, c);
-	xd::draw::traceline(dat, 0, 0, 13, 13, c);
-	xd::draw::traceline(dat, 13, 13, 13, 86, c);
-	xd::draw::traceline(dat, 13, 86, 0, 99, c);
+	xd::draw::tracepoly(dat, 0, 0,  {{0, 0}, {13, 13}, {13, 86}, {0, 99}},  c);
 	// right row1
-	// xd::draw::traceline(dat, 99, 0, 99, 99, c);
-	xd::draw::traceline(dat, 99, 0, 86, 13, c);
-	xd::draw::traceline(dat, 86, 13, 86, 86, c);
-	xd::draw::traceline(dat, 86, 86, 99, 99, c);
+	xd::draw::tracepoly(dat, 99, 0,  {{0, 0}, {-13, 13}, {-13, 86}, {0, 99}},  c);
 	// left row2
-	xd::draw::traceline(dat, 13, 13, 13, 86, c);
-	xd::draw::traceline(dat, 13, 13, 28, 28, c);
-	xd::draw::traceline(dat, 28, 28, 28, 71, c);
-	xd::draw::traceline(dat, 28, 71, 13, 86, c);
+	xd::draw::tracepoly(dat, 13, 13,  {{0, 0}, {15, 15}, {15, 58}, {0, 73}, {0, 0}},  c);
 	// right row2
-	xd::draw::traceline(dat, 99-13, 13, 99-13, 86, c);
-	xd::draw::traceline(dat, 99-13, 13, 99-28, 28, c);
-	xd::draw::traceline(dat, 99-28, 28, 99-28, 71, c);
-	xd::draw::traceline(dat, 99-28, 71, 99-13, 86, c);
+	xd::draw::tracepoly(dat, 99-13, 13,  {{0, 0}, {-15, 15}, {-15, 58}, {0, 73}, {0, 0}},  c);
 	// left row 3
-	xd::draw::traceline(dat, 28, 28, 28, 71, c);
-	xd::draw::traceline(dat, 28, 28, 37, 37, c);
-	xd::draw::traceline(dat, 37, 37, 37, 62, c);
-	xd::draw::traceline(dat, 37, 62, 28, 71, c);
+	xd::draw::tracepoly(dat, 28, 28,  {{0, 0}, {0, 43}, {9, 34}, {9, 9}, {0, 0}},  c);
 	// right row 3
-	xd::draw::traceline(dat, 99-28, 28, 99-28, 71, c);
-	xd::draw::traceline(dat, 99-28, 28, 99-37, 37, c);
-	xd::draw::traceline(dat, 99-37, 37, 99-37, 62, c);
-	xd::draw::traceline(dat, 99-37, 62, 99-28, 71, c);
+	xd::draw::tracepoly(dat, 99-28, 28,  {{0, 0}, {0, 43}, {-9, 34}, {-9, 9}, {0, 0}},  c);
 
 	while (true) {
 		if (xd::screen::paint())  break;
