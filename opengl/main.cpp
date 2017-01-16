f829273d6c64eddf372a78754830a2f9cb587f3e
doug <doug@d-mini.local>
2017-01-16 00:00:08 +0000

    sdl2

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+// #include <GL/glu.h>
+#include <GL/glew.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
-// #include <GL/glu.h>
 
 using namespace std;
 
 
 // globals
+int running = 1;
 SDL_Window* win = NULL;
 SDL_GLContext ctx;
 
@@ -37,13 +39,15 @@ int init() {
 	// set up perspective
 	// glMatrixMode( GL_PROJECTION );  /* change to the projection matrix and set our viewing volume. */
 	// glLoadIdentity();
-	// gluPerspective( 45.0f, 1.0f, 0.1f, 100.0f );  /* Set our perspective */
+	gluPerspective( 45.0f, 1.0f, 0.0f, -10.0f );  /* Set our perspective */
 	// glMatrixMode( GL_MODELVIEW );  /* Make sure we're chaning the model view and not the projection */
 	// glLoadIdentity();
 	
 	return 0;
 }
 
+
+int rot = 0;
 
 int draw() {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -53,8 +57,8 @@ int draw() {
     // glOrtho(-1, 1, 1, -1, 0, 10);
 	// glTranslatef(-1.5f, 0.0f, -6.0f);  // Move Left 1.5 Units And Into The Screen 6.0
 	// glTranslatef(0.5f, 0, 0);
-	glTranslatef(0, 0, -0.9f);
-	glRotatef(45, 0, 1, 0);
+	glTranslatef( 0, 0, -1.1 );
+	glRotatef(rot, 0, -1, 0);
 	glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
 		glColor3f(1.0f, 0.0f, 0.0f);
 		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top
@@ -71,8 +75,17 @@ int main() {
 	printf("start\n");
 	if (init())  return 1;
 
-	draw();
-	SDL_Delay(2000);
+	while (running) {
+		rot++;
+		draw();
+		SDL_Delay(16);
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+			switch (e.type) {
+			case SDL_QUIT:  running = 0;  break;
+			}
+		}
+	}
 
 	SDL_Quit();
 }
