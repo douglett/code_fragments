2cd37df600f9ccc0500a19da9fef2ee3f0b96c99
dougdoug <doug.inman@probability.co.uk>
2017-01-18 18:28:07 +0000

    opengl depth function

--- opengl/01_sdl2/main.cpp
+++ opengl/01_sdl2/main.cpp
@@ -20,26 +20,28 @@ int init() {
 		return 1;
 	}
 	// Create our window centered at 512x512 resolution
-	win = SDL_CreateWindow(
-		"My Game", 
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		512, 512,
-		SDL_WINDOW_OPENGL );
+	const auto  C = SDL_WINDOWPOS_CENTERED,  W = 512,  H = 512;
+	win = SDL_CreateWindow( "My Game", C, C, W, H, SDL_WINDOW_OPENGL );
 	if (!win) {
 		fprintf(stderr, "Failed to open SDL2 window\n");
 		return 1;
 	}
-	// set GL attributes
-	// SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK,   SDL_GL_CONTEXT_PROFILE_CORE );
-	// SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION,  3 );
-	// SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION,  2 );
+	
+	// set GL window attributes
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	ctx = SDL_GL_CreateContext(win);
+	ctx = SDL_GL_CreateContext(win);  // create window
+	
+	// set GL basic options
+	glEnable(GL_DEPTH_TEST);   // Enables Depth Testing (stuff in background)
+		// glDepthFunc(GL_LESS);
+		glDepthFunc(GL_GREATER);
+	// glEnable(GL_BLEND);  // transparency
+		// glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// set up perspective
 	glMatrixMode(GL_PROJECTION); 	// change to the projection matrix
 	glLoadIdentity();  				// reset matrix to identity position
-	gluPerspective(90.0f, 512/512, 0.1f, 100.0f);  // Set our perspective
+	gluPerspective(90.0f, float(W)/H, 0.1f, 100.0f);  // Set our perspective. this will show a warning that we can ignore
 	glMatrixMode(GL_MODELVIEW); 	// Make sure we're chaning the model from here on
 	glLoadIdentity(); 				// reset matrix to identity position
 
@@ -56,14 +58,27 @@ int draw() {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();  // reset matrix
 
-	glTranslatef( 0, 0, -2 );
+	glTranslatef( 0, 0, -6 );
 	glRotatef(rot, 0, -1, 0);
 	glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
-		glColor3f(1.0f, 0.0f, 0.0f);
+		// glColor3f(1.0f, 0.0f, 0.0f);
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top
 		glVertex3f(-1.0f,-1.0f, 0.0f);          // Bottom Left
 		glVertex3f( 1.0f,-1.0f, 0.0f);          // Bottom Right
-	glEnd();  	
+	glEnd();
+
+	glLoadIdentity();
+	glTranslatef( 0, 0, -3 );
+	glRotatef(rot+90, 0, -1, 0);
+	glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
+		// glColor3f(1.0f, 0.0f, 0.0f);
+		glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top
+		glVertex3f(-1.0f,-1.0f, 0.0f);          // Bottom Left
+		glVertex3f( 1.0f,-1.0f, 0.0f);          // Bottom Right
+	glEnd();
+	// glLoadIdentity();
 
 	SDL_GL_SwapWindow(win);
 	return 0;
