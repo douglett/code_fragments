3d7d3c62090fee292da6da4c1e30819c7d0f7de3
dougdoug <doug.inman@probability.co.uk>
2017-01-19 10:41:27 +0000

    drawing update

--- opengl/01_sdl2/main.cpp
+++ opengl/01_sdl2/main.cpp
@@ -32,17 +32,14 @@ int init() {
 	ctx = SDL_GL_CreateContext(win);  // create window
 	
 	// set GL basic options
-	glEnable(GL_DEPTH_TEST);   // Enables Depth Testing (stuff in background)
-		// glDepthFunc(GL_LESS);
-		glDepthFunc(GL_GREATER);
-	// glEnable(GL_BLEND);  // transparency
-		// glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST),  glDepthFunc(GL_LESS);  // stuff in background
+	glEnable(GL_BLEND),       glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  // transparency
 
 	// set up perspective
 	glMatrixMode(GL_PROJECTION); 	// change to the projection matrix
 	glLoadIdentity();  				// reset matrix to identity position
 	gluPerspective(90.0f, float(W)/H, 0.1f, 100.0f);  // Set our perspective. this will show a warning that we can ignore
-	glMatrixMode(GL_MODELVIEW); 	// Make sure we're chaning the model from here on
+	glMatrixMode(GL_MODELVIEW); 	// Make sure we're changing the model from here on
 	glLoadIdentity(); 				// reset matrix to identity position
 
 	// set some defaults
@@ -55,14 +52,14 @@ int init() {
 int rot = 0;
 
 int draw() {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();  // reset matrix
 
 	glTranslatef( 0, 0, -6 );
 	glRotatef(rot, 0, -1, 0);
 	glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
 		// glColor3f(1.0f, 0.0f, 0.0f);
-		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		glColor4f(1.0f, 0.0f, 0.0f, 0.5f);
 		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top
 		glVertex3f(-1.0f,-1.0f, 0.0f);          // Bottom Left
 		glVertex3f( 1.0f,-1.0f, 0.0f);          // Bottom Right
@@ -72,8 +69,8 @@ int draw() {
 	glTranslatef( 0, 0, -3 );
 	glRotatef(rot+90, 0, -1, 0);
 	glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
-		// glColor3f(1.0f, 0.0f, 0.0f);
-		glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+		// glColor3f(0.0f, 0.0f, 1.0f);
+		glColor4f(0.0f, 0.0f, 1.0f, 0.5f);
 		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top
 		glVertex3f(-1.0f,-1.0f, 0.0f);          // Bottom Left
 		glVertex3f( 1.0f,-1.0f, 0.0f);          // Bottom Right
@@ -97,6 +94,12 @@ int main() {
 		while (SDL_PollEvent(&e)) {
 			switch (e.type) {
 			case SDL_QUIT:  running = 0;  break;
+			case SDL_KEYDOWN:
+				switch (e.key.keysym.sym) {
+				case SDLK_ESCAPE:  running = 0;  break;
+				default:  printf("%d\n", e.key.keysym.sym);  break;
+				}
+				break;
 			}
 		}
 	}
