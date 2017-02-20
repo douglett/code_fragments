59b177b9c6c5c384c8b6be15456162677fde115f
dougdoug <doug.inman@probability.co.uk>
2017-02-20 11:50:22 +0000

    testing

--- dlrun/test_01/main.cpp
+++ dlrun/test_01/main.cpp
@@ -17,14 +17,26 @@ int main() {
 }
 
 
+template<typename ... Args>
+string strfmt(const string& fmt, Args ... args) {
+	int size = snprintf(NULL, 0, fmt.c_str(), args ...) + 1; // Extra space for '\0'
+	char cs[size];
+	snprintf(cs, size, fmt.c_str(), args ...);
+	return cs;
+}
+
+
 int sdl2_test() {
 	pipelib::dblock_t data;
 
 	pipelib::dlpath = "./libs/";
 	if (pipelib::loadall({ "bmpload", "sdl2test" }))  return 1;
 
+	// cout << strfmt("cls %02x%02x%02x%02x", 0x88, 0x00, 0x00, 0xff) << endl;
+	// cout << strfmt("cls %08x", 0x880000ff) << endl;
+
 	pipelib::send("sdl2test", "init");
-	pipelib::send("sdl2test", "cls 0xff0000ff");
+	pipelib::send("sdl2test", "cls 0x880000ff");
 	if (pipelib::send("bmpload", "load asd.bmp", &data))  return 1;  // load into slot 1
 	pipelib::push32(data, { 20, 40 });  // blit data in slot 1, to pos in slot 2
 	pipelib::send("sdl2test", "blit", &data);  // do blit
