8def4967cadc27638ed98accc057afcc037d5903
doug <doug@d-mini.local>
2017-09-23 16:57:47 +0100

    getch

--- dterm/main.cpp
+++ dterm/main.cpp
@@ -11,7 +11,8 @@ std::list<std::string>& PIPE_1() {
 	static list<string> ls={
 		"locate 1 1",
 		"printf \"hello world\\n\"",
-		"getline" };
+		"getch"
+	};
 	return ls;
 }
 void PIPE_2(std::list<std::string>& ls) {
@@ -35,22 +36,43 @@ vector<string> split(const string& ln) {
 }
 
 
+list<string> response;
+
+
+int getch() {
+	int rval=0;
+	SDL_Event e;
+	while (SDL_PollEvent(&e)) {
+		if (e.type==SDL_QUIT) rval=-1;
+		if (e.type==SDL_KEYDOWN) 
+			switch (e.key.keysym.sym) {
+			case 27:  rval=-1;  break;
+			default:  if (!rval) rval=e.key.keysym.sym;
+			}
+	}
+	return rval;
+}
+
+
 int main(int argc, char** argv) {
 	printf("start\n");
 
 	gfx::init(640, 480, "win");
-	// gfx::handleEvents=0;
+	gfx::handleEvents=0;
 	SDL_Surface* scr = gfx::mksprite(320, 240);
 	SDL_FillRect(scr, NULL, 0x000000ff);
 
 	int termw=39, termh=29, termx=0, termy=0;
 
-	int loop=1, err=0;
-	while (loop && !err) {
+	int err=0, getchk=0, shown=1;
+	while (err==0 && getchk!=-1) {
 		auto& inp = PIPE_1();  // get input from the master pipe
 		while (inp.size()) {
 			auto vs=split( inp.front() );
-			for (auto& s : vs) printf("  [%s]\n", s.c_str());
+			if (!shown) {
+				for (auto& s : vs) printf("  [%s]\n", s.c_str());
+				shown=1;
+			}
 			if      (vs.size()==0)  continue;
 			else if (vs[0]=="locate" && vs.size()==3) {
 				termx = max(min((int)stol(vs[1]), termw), 0);
@@ -60,16 +82,23 @@ int main(int argc, char** argv) {
 			else if (vs[0]=="printf" && vs.size()>=2) {
 				gfx::drawstr(scr, 4+termx*8, 4+termy*10, vs[1]);
 			}
+			else if (vs[0]=="getch") {
+				if (!getchk) break;
+				printf("getch: %d\n", getchk);
+				response.push_back("getch "+to_string(getchk));
+			}
 			else {
 				fprintf(stderr, "error: %s\n", inp.front().c_str());
-				// err=1;  break;
+				err=1;  break;
 			}
 			inp.pop_front();
+			shown=0;
 		}
 		// paint
 		SDL_FillRect(SDL_GetVideoSurface(), NULL, 0x000000ff);
 		gfx::scale2x(scr, SDL_GetVideoSurface());
-		if (gfx::flip())  break;
+		getchk=getch();
+		gfx::flip();
 	}
 
 	SDL_Quit();
