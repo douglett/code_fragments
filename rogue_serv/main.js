6b8a16a6350eada2c8651069e9f3e795b150b5b0
Douglas Inman <Douglas.Inman@strategic-i.com>
2018-09-13 11:09:14 +0100

    working on player movement

--- rogue_serv/main.js
+++ rogue_serv/main.js
@@ -39,6 +39,26 @@ function serve(request, response) {
 			response.end(html);
 		});
 		break;
+	case "/action.json": {
+		// get initial player data
+		let pdata = gameplayers[0];
+		// do action
+		console.log(url.query);
+		if (url.query.walk) {
+			switch (url.query.walk) {
+			case 'n':  pdata.y--;  break;
+			case 's':  pdata.y++;  break;
+			case 'e':  pdata.x++;  break;
+			case 'w':  pdata.x--;  break;
+			}
+		}
+		// draw map into response
+		pdata.map = getmapdata(pdata);
+		// return player object and position
+		response.writeHead(200, {"Content-Type": "text/json"});
+		response.write(JSON.stringify(pdata));
+		response.end();
+		} break;
 	case "/favicon.ico":
 		send404(response);
 		break;
@@ -53,4 +73,44 @@ function send404(response) {
 	response.writeHead(404, {'Content-Type': 'text/html'});
 	response.write("404: not found");
 	response.end();
+}
+
+
+let gameplayers = [{
+	id: "player1",
+	x: 2,
+	y: 2
+}];
+let gamemap = [
+	"##########             ",
+	"#        #             ",
+	"#        ##########    ",
+	"#                 #    ",
+	"#        ######## #    ",
+	"##########      # #    ",
+	"                # #    ",
+	"                # #    ",
+	"                # #    ",
+	"            ##### #####",
+	"            #         #",
+	"            #         #",
+	"            #         #",
+	"            ###########",
+];
+function getmapdata(pdata) {
+	let map = {
+		camw: 9,
+		camh: 9,
+		camx: 0,
+		camy: 0,
+		data: JSON.parse(JSON.stringify(gamemap))
+	};
+	// add player
+	let r = map.data[pdata.y];
+	map.data[pdata.y] = r.substr(0, pdata.x) + '@' + r.substr(pdata.x+1);
+	// update camera info
+	// pdata.camx = pdata.x - (pdata.camw/2|0);
+	// pdata.camy = pdata.y - (pdata.camh/2|0);
+	// OK
+	return map;
 }
