1b055fbd6b25d93ab85468aee2fc7d022836b3f0
Douglas Inman <Douglas.Inman@strategic-i.com>
2018-09-14 17:45:03 +0100

    map update

--- rogue_serv/main.js
+++ rogue_serv/main.js
@@ -46,14 +46,14 @@ function serve(request, response) {
 		console.log(url.query);
 		if (url.query.walk) {
 			switch (url.query.walk) {
-			case 'n':  pdata.y--;  break;
-			case 's':  pdata.y++;  break;
-			case 'e':  pdata.x++;  break;
-			case 'w':  pdata.x--;  break;
+			case 'n':  if (!gamemap.checkMapCollision(pdata.x, pdata.y-1))  pdata.y--;  break;
+			case 's':  if (!gamemap.checkMapCollision(pdata.x, pdata.y+1))  pdata.y++;  break;
+			case 'e':  if (!gamemap.checkMapCollision(pdata.x+1, pdata.y))  pdata.x++;  break;
+			case 'w':  if (!gamemap.checkMapCollision(pdata.x-1, pdata.y))  pdata.x--;  break;
 			}
 		}
 		// draw map into response
-		pdata.map = getmapdata(pdata);
+		pdata.map = gamemap.getMapData(pdata);
 		// return player object and position
 		response.writeHead(200, {"Content-Type": "text/json"});
 		response.write(JSON.stringify(pdata));
@@ -81,36 +81,52 @@ let gameplayers = [{
 	x: 2,
 	y: 2
 }];
-let gamemap = [
-	"##########             ",
-	"#        #             ",
-	"#        ##########    ",
-	"#                 #    ",
-	"#        ######## #    ",
-	"##########      # #    ",
-	"                # #    ",
-	"                # #    ",
-	"                # #    ",
-	"            ##### #####",
-	"            #         #",
-	"            #         #",
-	"            #         #",
-	"            ###########",
-];
-function getmapdata(pdata) {
-	let map = {
-		camw: 9,
-		camh: 9,
-		camx: 0,
-		camy: 0,
-		data: JSON.parse(JSON.stringify(gamemap))
-	};
-	// add player
-	let r = map.data[pdata.y];
-	map.data[pdata.y] = r.substr(0, pdata.x) + '@' + r.substr(pdata.x+1);
-	// update camera info
-	// pdata.camx = pdata.x - (pdata.camw/2|0);
-	// pdata.camy = pdata.y - (pdata.camh/2|0);
-	// OK
-	return map;
-}
+let gamemap = {
+	gamemap: [
+		"##########             ",
+		"#        #             ",
+		"#        ##########    ",
+		"#                 #    ",
+		"#        ######## #    ",
+		"##########      # #    ",
+		"                # #    ",
+		"                # #    ",
+		"                # #    ",
+		"            ##### #####",
+		"            #         #",
+		"            #         #",
+		"            #         #",
+		"            ###########",
+	],
+	getMapData: function(pdata) {
+		let map = {
+			camw: 9,
+			camh: 9,
+			camx: 0,
+			camy: 0,
+			data: JSON.parse(JSON.stringify(this.gamemap))
+		};
+		// add player
+		let r = map.data[pdata.y];
+		map.data[pdata.y] = r.substr(0, pdata.x) + '@' + r.substr(pdata.x+1);
+		// update camera info
+		// pdata.camx = pdata.x - (pdata.camw/2|0);
+		// pdata.camy = pdata.y - (pdata.camh/2|0);
+		// OK
+		return map;
+	},
+	checkInBounds: function(x, y) {
+		return ( x >= 0 && y >= 0 && y < this.gamemap.length && x < this.gamemap[y].length );
+	},
+	checkMapCollision: function(x, y) {
+		if (!this.checkInBounds)  return false;
+		return ( this.gamemap[y][x] !== ' ' );
+	},
+	// should these be in here?
+	checkMobCollision: function(x, y) {
+		return false;
+	},
+	checkPlayerCollision: function(x, y) {
+		return false;
+	}
+};
